ea2baa1908b83f6e45b296ecd15175e514762e48
Meskius <dziuras123@gmail.com>
2017-12-09 15:45:48 +0200

    Title uzdeti
    Signed-off-by: Meskius <dziuras123@gmail.com>

--- src/projektinis/Main.java
+++ src/projektinis/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application  {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        primaryStage.setTitle("TEst");
+        primaryStage.setTitle("Kompiuterių detalių prekybos programga");
         GridPane gridPane = new GridPane();
 
         {
